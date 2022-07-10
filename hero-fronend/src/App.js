19619ef106a764bb6f5a9fbd149e6732937b3aa6
khaldi505 <ihebkhaldi505@gmail.com>
2022-07-10 10:11:00 +0100

    Homepage style fix

--- hero-fronend/src/App.js
+++ hero-fronend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import maskGroup from './Assets/MobilizersPhotos/Mask_group.png';
 import logo from './Assets/HERO Brand Assets/logo.png'
 import { Link } from "react-router-dom";
+import herodot from './Assets/HERO Brand Assets/g4622.png'
 function App() {
   return (
     <div className="App container">
@@ -11,15 +12,27 @@ function App() {
       <h1 className='headline'>
       Do you want to do more for the climate but just don’t know how?
       </h1>
-      <p className='headline-desc'>HERO gives you the power to take meaningful action in just 3 clicks by financing climate mobilizers around the world with the most impact on policy today. </p>
+      <p className='headline-desc my-1'>HERO gives you the power to take meaningful action in just 3 clicks by financing climate mobilizers around the world with the most impact on policy today. </p>
 
-      <ul className='d-flex flex-column'>
+      <ul className='d-flex flex-column my-1 mb-4'>
         <li>
-        No intermediaries
+          <span className="d-flex flex-column">
+          <img src={herodot} srcset={herodot} className="herodot mx-auto"/>
+          <p className="mx-auto">No intermediaries</p>
+          
+          </span>
         </li>
-      <li>Full transparency</li>
+      <li>
+      <span className="d-flex flex-column">
+      <img src={herodot} srcset={herodot} className="herodot mx-auto"/>
+      <p className="mx-auto">Full transparency</p>
+      </span>
+        </li>
         <li>
-        Vote on the future of HERO
+        <span className="d-flex flex-column">
+      <img src={herodot} srcset={herodot} className="herodot mx-auto"/>
+      <p className="mx-auto">Vote on the future of HERO</p>
+      </span>
         </li>
       </ul>
 
