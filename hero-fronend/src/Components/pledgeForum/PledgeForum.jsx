e9328bd68864294971b6522a60e4d4b8df942f26
khaldi505 <ihebkhaldi505@gmail.com>
2022-07-10 09:55:56 +0100

    Homepage, Supporter still needs spacing fix

--- hero-fronend/src/Components/pledgeForum/PledgeForum.jsx
+++ hero-fronend/src/Components/pledgeForum/PledgeForum.jsx
@@ -16,6 +16,70 @@ const PledgeForum = () => {
         <p className="p-end">We will let you know as soon as HERO is officially launched. You will then be part of the first HERO Supporters  in the world.</p>
 
       </div>
+      <div className="d-flex flex-column justify-content-center align-items-center my-5 "> 
+        <img id='logo' src={logo} srcSet={logo} alt="logo" />
+        <p className="p-headline">
+        Which aspects of climate change concern you the most?        
+        </p>
+        <button className="btn-c-m">Drought (water scarcity)</button>
+        <button className="btn-c-m w my-4">Severe weather/natural disasters</button>
+        <button className="btn-c-m l">Social unrest (due to worsened economic conditions, food scarcity, etc.)</button>
+
+        <button className="btn btn-transparent"><img src={play} srcSet={play} alt="submit" /></button>
+      </div>
+      <div className="d-flex flex-column justify-content-center align-items-center my-5 "> 
+        <img id='logo' src={logo} srcSet={logo} alt="logo" />
+        <p className="p-headline">
+        What excites you more about HERO?</p>
+        <button className="btn-c-m">The ability to contribute directly to a climate mobilizer</button>
+        <button className="btn-c-m w mt-4">Having a vote on the governance and future of HERO</button>
+        <button className="btn-c-m w my-4">Getting direct feedback on the actions of the mobilizer</button>
+        <button className="btn-c-m l">Access exclusive rewards and experiences from sustainable brands and partners</button>
+
+        <button className="btn btn-transparent"><img src={play} srcSet={play} alt="submit" /></button>
+      </div>
+
+
+
+
+
+{/* Contact */}
+      <div className="d-flex flex-column justify-content-center align-items-center my-5 "> 
+        <img id='logo' src={logo} srcSet={logo} alt="logo" />
+        <form className="d-flex flex-column align-self-center align-items-center justify-content-center">        
+        <label>
+          <h6>Your Name</h6>
+          <input type="text" placeholder="Full Name" />
+        </label>
+        <label>
+        <h6>Email</h6>
+          <input type="text" placeholder="Your email adresss" />
+        </label>
+        <label>
+        <h6>Birth Date</h6>
+          <input type="text" placeholder="Your Birth Date" />
+        </label>
+        <label>
+        <h6>City</h6>
+          <input type="text" placeholder="Your City" />
+        </label>
+        <button className="btn-c-m w my-4">Become a HERO Supporter</button>
+      </form>
+      </div>
+
+
+
+
+      <div className="d-flex flex-column justify-content-center align-items-center my-5 "> 
+        <img id='logo' src={logo} srcSet={logo} alt="logo" />
+        <h6 className="wtc">Welcome to the future of climate action!</h6>
+      </div>
+
+
+
+
+
+
     </div>
   )
 }
