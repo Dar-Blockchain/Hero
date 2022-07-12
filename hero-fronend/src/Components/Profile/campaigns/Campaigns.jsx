09e4af50adf17eb23cffa399345aef40493ca11f
khaldi505 <ihebkhaldi505@gmail.com>
2022-07-12 10:25:20 +0100

    Campaigns close btn fix

--- hero-fronend/src/Components/Profile/campaigns/Campaigns.jsx
+++ hero-fronend/src/Components/Profile/campaigns/Campaigns.jsx
@@ -3,12 +3,16 @@ import { MobilizerData } from '../../../Mobilizer_information';
 import personn from '../../../Assets/HERO Brand Assets/Group 2.svg'
 import leaf from '../../../Assets/HERO Brand Assets/Group 1.svg'
 const Campaigns = ({profileCmp}) => {
-
+  const closeHandler = (e) => {
+    e.preventDefault();
+    window.location.reload()
+    
+  }
   return (
     <>
     {/* onClick={closeCamp} */}
     <div className="container TopCampaigns d-flex flex-column">
-          <button className="closebtn align-self-end">X</button>
+          <button name={profileId} onClick={closeHandler} className="closebtn align-self-end">X</button>
           <h5 className="ccmp mx-auto">Current Campaigns</h5>
           <div className='d-flex flex-column justify-content-center align-items-center' >
           {profileCmp && 
